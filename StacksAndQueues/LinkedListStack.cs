63b29f75544191a6e22363be2818d696f220b91d
Chetan Deore <deorec346@gmail.com>
2022-04-25 11:35:37 +0530

    StacksAndQues

---
+++ StacksAndQueues/LinkedListStack.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    public class LinkedListStack
+    {
+        private Node top;
+
+        //Constructor
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+
+        //Push Data to stack
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+
+            }
+            this.top = node;
+
+            //Display Added Data value
+            Console.WriteLine("{0} pushed  to Stack : ", value);
+        }
+
+        //Display Data of Top
+        internal void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is Empty !");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of the Stack : ", this.top.data);
+        }
+
+        //Delee data from Top of the Stack
+        internal void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is Empty, Deletion is not Possible !");
+                return;
+            }
+            Console.WriteLine("Popped value : {0}", this.top.data);
+            Console.WriteLine("----------------------------");
+            this.top = this.top.next;
+        }
+
+        //Deletion till stack Will Empty
+        internal void IsEmpty()
+        {
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+        }
+
+        //Display Stack
+        internal void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine("Stack data : " + temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Message
+            Console.WriteLine("=-=-=-=-=-Welcome To The Stacks And Queues Program-=-=-=-=-=");
+            Console.WriteLine("----------------------------");
+
+            /*//Calling Class and Adding Data in Stack
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            Console.WriteLine("----------------------------");
+            
+            //Peek Value from Stack
+            linkedListStack.Peek();
+            Console.WriteLine("----------------------------");
+            linkedListStack.Pop();
+            //Pop value till Stack will empty
+            linkedListStack.IsEmpty();
+*/
+            //Calling Class and Adding Data in Stack
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            Console.WriteLine("----------------");
+
+            //Display Queue
+            linkedListQueue.Display();
+            Console.WriteLine("----------------");
+
+            //Dequeue from queue
+            linkedListQueue.Dequeue();
+
+            //Empty Dequeue
+            linkedListQueue.IsEmpty();
+
+            //Display Queue
+            linkedListQueue.Display();
+            Console.WriteLine("----------------");
+
+            Console.ReadLine();
+        }
+    }
+}
